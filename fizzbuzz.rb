7e8eea3910c153cafd22d8791eebe351cc33e427
Samidh Patel <spatel13456@gmail.com>
2019-01-05 19:41:45 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,10 +5,12 @@ def fizzbuzz(int)
     return "Fizz"
   elsif int % 5 == 0
     return "Buzz"
-  elsif int % 3 == 0 && int % 5 ==  0
-    return "FizzBuzz"
- else 
+  else 
    return nil;
+   
+  if int % 3 == 0 && int % 5 ==  0
+    return "FizzBuzz"
+ 
   end
 end
 
