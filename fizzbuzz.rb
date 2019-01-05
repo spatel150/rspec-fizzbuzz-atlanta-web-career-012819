e9cfcbff11e0f27c1cdecbac35bc33db5bb81682
Samidh Patel <spatel13456@gmail.com>
2019-01-05 19:43:16 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,8 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
-  if 
+  if int % 3 == 0 && int % 5 ==  0
+    return "FizzBuzz"
   elsif int % 3 == 0
     return "Fizz"
   elsif int % 5 == 0
@@ -9,8 +10,7 @@ def fizzbuzz(int)
   else 
    return nil;
  
-  if int % 3 == 0 && int % 5 ==  0
-    return "FizzBuzz"
+  if
 end
 
 puts(fizzbuzz(15))
